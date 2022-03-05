66f0e93ebc093fcdc613c1d969d309fadf46d577
maks <65305777+milkyhack@users.noreply.github.com>
2022-03-05 20:05:06 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,160 @@
+// // number , string , object , array , undefined , symbol , Boolean
+// //методы массива 
+// //1.map
+// //sort , fined index , filter , for each 
+
+// //funtions
+
+// //задание 1
+// console.log(
+//     [4, 4, 2, 2, 1, 5]
+
+//         .filter(element => !(element % 2))
+
+//         .reduceRight((maks, element) => maks + Math.sqrt(element), 0)
+// );
+
+// //задание 2
+
+// //a - яблока , b - апельсин
+
+// let a = 1.15
+
+// let b = 2.30
+
+// let c
+
+// c = a + b
+
+// console.log(c)
+
+// //задача 3 
+// let d = '*';
+// let g = 0;
+// do {
+//     g = g + 1
+//     if (g < 6);
+//     console.log(d)
+//     d = d + '*'
+// } while (g < 6)
+
+// // задание 4 buzz and fizz
+
+// let k = 0
+// let u = 0
+// do {
+//     k = k + 1;
+//     u = u + 1;
+//     if (k % 3 == 0)
+//         console.log("buzz" + ' ' + u)
+//     if (k % 5 == 0)
+//         console.log("fizz" + '' + u)
+//     if (k % 3 == 0 && k % 5 == 0)
+//         console.log("buzzfizz")
+// } while (k < 101)
+
+// //задача 5
+// let chas = 1
+// let min = 60
+// let sec = 60
+// console.log(min * sec * chas + '' + 'seconds')
+
+// //задача 6 
+// // sign = prompt("what you name?")
+// // a = sign
+// // if (sign.toLowerCase() == a) {
+// //     alert("Wow ,YOU ARE REALLY" + " " + a)
+// // }
+
+// //задача 7
+
+// let str = ["a","b","c","d","e"]
+
+// console.log( str.length )
+
+// console.log (str[0], str[2], str[4])
+
+// //задача 8 
+// var obj = {'Коля':'1000', 'Вася':'500','Петя':'200'};
+
+// console.log("Nikolai"+" "+obj['Коля']+" "+"Petya"+" "+obj['Петя'])
+
+// //задача 9 
+
+// var arr = ['2','5','3','9']
+// k = arr[0];
+// f = arr[1];
+// l = arr[2];
+// s = arr[3];
+// one = k*f;
+// two = l*s;
+// result = one + two ;
+// console.log(result)
+
+// //задача 10 
+
+// var arr = [[1,2,3],[4,5,6],[7,8,9]]
+// console.log(arr[1][0])
+
+// //задача 11
+
+//   m = parseInt(0);
+//   n = parseInt(59);
+//   min =  Math.floor( Math.random() * (n - m + 1) ) + m;
+// console.log(min)
+
+// if (min <= 15 && min >= 0 ) 
+//     console.log( "1")
+// if(min <= 30 && min > 15)
+//     console.log ("2")
+// if(min <= 45 && min > 35)
+//     console.log ("3")
+// if(min <= 60 && min > 45)
+//     console.log ("4")
+
+// // задача 12
+
+//  a = 1 
+//  if (a<0)
+// console.log ("ok")
+// else console.log("oh,no")
+
+// a = 0
+// if (a<0)
+    // console.log ("ok")
+// else console.log("oh,no")
+
+// a = -3
+// if (a<0)
+    // console.log ("ok")
+// else console.log("oh,no")
+// // or использовать массив и индекс 
+// // var arr = [1,0,-3]
+// // s = 0 
+// // a = arr[s]
+// // k = 0
+// // do {
+// // k = k +1 ; 
+// // if (a<0)
+    // // console.log("ok")
+// // else console.log ("oh, no")
+// // s = s + 1 ;
+// // a = arr[s];
+// // } while (k<3)
+
+// задача 13 
+
+var test = true 
+if (test = true )
+    console.log ("ok")
+ else console.log ("oh, no")
+
+// кратко 
+
+var test = false 
+    if (test) console.log ("ok1")
+else console.log ("oh, no")
+
+// задача 14 
+
+ a = 2 
